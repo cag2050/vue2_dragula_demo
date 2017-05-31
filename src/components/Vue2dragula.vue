ffe2640fb26d42c6ce0f20c5c243d3182ee623fc
cag2050 <cag2050@163.com>
2017-05-31 18:50:38 +0800

    m

--- src/components/Vue2dragula.vue
+++ src/components/Vue2dragula.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="app">
+<div id="app">
     <div class="container">
-      <div name="fade" tag="div" v-dragula="items" drake="items">
-        <div class="item" v-for="(item, index) in items" :key="item"><img :src="item" width="200px" height="130px"></div>
-      </div>
+        <div name="fade" tag="div" v-dragula="items" drake="items">
+            <div class="item" v-for="(item, index) in items" :key="item"><img :src="item" width="200px" height="130px"></div>
+        </div>
     </div>
 </div>
 </template>
@@ -16,51 +16,48 @@ import scene4 from '../assets/scene4.jpg'
 import scene5 from '../assets/scene5.jpg'
 
 export default {
-  data () {
-    return {
-        items: [scene1, scene2, scene3, scene4, scene5 ]
+    data () {
+        return {
+            items: [ scene1, scene2, scene3, scene4, scene5 ]
+        }
+    },
+    created () {
+        let service = this.$dragula.$service
+        service.options('items', {direction: 'horizontal'})
     }
-  },
-  created () {
-    let service = this.$dragula.$service
-    service.options('items', {direction: 'horizontal'})
-  }
 }
 </script>
 
 <style>
 .gu-mirror {
-  position: absolute;
-  pointer-events: none;
-  transition: all 0s !important;
+    position: absolute;
+    pointer-events: none;
+    transition: all 0s !important;
 }
 .gu-transit {
-  color:white;
-    width: 198px;
-    height: 128px;
-  border:1px dotted blue;
-  box-sizing: border-box;
-  opacity:1;
+    color:white;
+    border:1px dotted blue;
+    box-sizing: border-box;
+    opacity:1;
+}
+.gu-transit img {
+    display:none;
 }
 
 .item {
-  display: inline-block;
-  width: 200px;
-  height: 130px;
-  background: blue;
-  //padding: 5px;
-  margin: 5px;
-  //border: 1px white solid;
-  cursor: move;
- // background: white;
-  transition: all 1s;
-  text-align: center;
+    display: inline-block;
+    width: 200px;
+    height: 130px;
+    margin-left: 5px;
+    cursor: move;
+    transition: all 1s;
+    text-align: center;
 }
 
 .fade-enter,
 .fade-leave-active {
-  opacity: 0;
-  transform: translateY(30px);
+    opacity: 0;
+    transform: translateY(30px);
 }
 
 .fade-leave-active {
